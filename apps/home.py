b1697391d7f65b118aff58430cc28081bed222b3
Ben Elsworth <be15516@bristol.ac.uk>
2021-04-20 12:20:23 +0100

    added tsne data and plot

--- apps/home.py
+++ apps/home.py
@@ -5,7 +5,9 @@ import dash_bootstrap_components as dbc
 import dash_table
 from dash.dependencies import Input, Output, State
 import plotly.express as px
+import plotly.graph_objects as go
 import requests
+import numpy as np
 import pandas as pd
 from app import app, navbar
 from loguru import logger
@@ -16,15 +18,27 @@ app = dash.Dash(external_stylesheets = [dbc.themes.BOOTSTRAP])
 app.title = "Researcher Searcher - UoB Data Science Network"
 
 df = pd.read_csv('data/tsne.csv.gz')
+df.drop_duplicates(inplace=True)
+org_counts = df['org-name'].value_counts()
 
-def plotly_scatter_plot(df):
+# filter df to top X for plot
+df['org_count']= df['org-name'].map(org_counts)
+df['org-name'] = df['org-name']+' '+df['org_count'].astype(str)
+
+logger.info(f'\n{org_counts}')
+logger.info(f'\n{df}')
+
+def plotly_scatter_plot(df,top=12):
+    df = df[df['org_count']>top]
     fig = px.scatter(
         df, 
         x="x", 
         y="y", 
         color="org-name",
+        symbol="org-name",
         hover_data=['email']
         )
+    fig.update_layout(legend_title_text=f'Organisation (>{top} people)')
     return fig
 
 fig = plotly_scatter_plot(df)
@@ -34,7 +48,8 @@ layout = html.Div([
         dbc.Container([
             dcc.Graph(
                 figure=fig,
-                responsive=True
+                responsive=True,
+                style={'height': '80vh'}
             )
         ])
     ])
