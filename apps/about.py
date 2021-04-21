f8d77b9a2ef58bd17fabf49cb97fe31cd810bec6
Ben Elsworth <be15516@bristol.ac.uk>
2021-04-21 22:07:20 +0100

    more docs

--- apps/about.py
+++ apps/about.py
@@ -27,33 +27,34 @@ layout = html.Div([
                     
                     1. **Full text:** Query text is split into sentences. 
                     Each sentence is queried against each title/abstract sentences using the 
-                    Elasticsearch full text method (limit top 100).  Weighted average returned for 
+                    Elasticsearch full text [similarity score](https://www.elastic.co/blog/practical-bm25-part-2-the-bm25-algorithm-and-its-variables) 
+                    (limit top 100).  Weighted average returned for 
                     each person using score and rank.
                     
                     2. **Vector:** Query text is split into sentences. 
-                    A vector representation of each sentence is created then 
-                    compared to the vectors of all title/abstract sentences 
+                    A vector\* representation of each sentence is created then 
+                    compared to the vectors\* of all title/abstract sentences 
                     using cosine distance (limit top 100).  Weighted average 
                     returned for each person using distance and rank.
                     
                     3. **Person:** Query text is treated as single document 
-                    and a single vector is created. This is compared to each 
-                    person vector (average of output sentences) using cosine 
+                    and a single vector\* is created. This is compared to each 
+                    person vector\* (average of output sentences) using cosine 
                     distance and top 100 returned.
                     
                     4. **Output:** Query text is treated as single document and a 
-                    single vector is created. This is compared to each output vector 
+                    single vector\* is created. This is compared to each output vector\* 
                     (average of output sentences) using cosine distance and top 100 
                     returned.
 
                     ### Person
 
-                    For a given person, returns a summary of noun chunks identified from 
-                    all associated output using the [TF-IDF](https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html) score.
+                    For a given person, returns a summary of [Spacy noun chunks](https://spacy.io/usage/linguistic-features#noun-chunks) identified from 
+                    all associated sorted by the [TF-IDF](https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html) score.
 
                     ### Collaboration
 
-                    For a given person, returns the most similar people as defined by average sentence vectors. 
+                    For a given person, returns the most similar people as defined by average sentence vectors\*. 
                     
                     ##### Three filter options:
                     
@@ -64,6 +65,8 @@ layout = html.Div([
                     3. **All:** Return all people
                     
                     ***
+
+                    \*All vectors were created using [Google Universal Sentence Encoder via Spacy](https://spacy.io/universe/project/spacy-universal-sentence-encoder)
 
                     For more information on the construction of this data set, see 
                     this GitHub repository - https://github.com/elswob/researcher-searcher-jgi
