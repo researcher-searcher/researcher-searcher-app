9d66664285735c6e8e927df438eaaf1724aa19fa
Ben Elsworth <be15516@ieu-mrbssd1.epi.bris.ac.uk>
2021-04-28 15:13:22 +0100

    modified app to allow static files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 #app = dash.Dash(__name__, suppress_callback_exceptions=True)
-app = dash.Dash(external_stylesheets = [dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__,external_stylesheets = [dbc.themes.BOOTSTRAP])
 app.title = 'Researcher Searcher'
 
 navbar = dbc.NavbarSimple(
