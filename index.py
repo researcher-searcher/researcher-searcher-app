cef1db24dbf155941f3cf9d59fcc572fcf0f60b3
Ben Elsworth <elswob@gmail.com>
2022-07-10 23:11:13 +0100

    autocomplete api call

--- index.py
+++ index.py
@@ -105,19 +105,19 @@ def run_search(n_clicks, slider_val, value, input1, input2):
     Input("person-submit-button-state", "n_clicks"),
     #Input("person-submit-button-state", "value"),
     Input({"id": 'dest-loc', "type": "searchData"}, "value"),
-    State("person-input-1-state", "value"),
+    State("dest-loc", "value"),
     prevent_initial_call=True
 )
 def run_person(n_clicks, value, input1):
     if n_clicks == 0:
         return dash.no_update
     else:
-        df = api_person(text=input1)
         if len(value) < 4:
             raise dash.exceptions.PreventUpdate
         else:
             df2 = api_lookup(text=input1)
             print(df2)
+            df = api_person(text=input1)
         try:
             return df.to_dict("records"), value, df2.to_dict("records")
         except:
